3dc6fdfc99aec35590331e042b526c1aa27c621e
Andrew Klmn <andrew.klmn@gmail.com>
2021-06-09 22:18:00 +0300

    Add: meteor app

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,17 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useTracker } from 'meteor/react-meteor-data';
+import { TasksCollection } from '/imports/api/TasksCollection';
 import { Task } from './Task';
+import { TaskForm } from './TaskForm';
 
-const tasks = [
-  {_id: 1, text: 'First Task'},
-  {_id: 2, text: 'Second Task'},
-  {_id: 3, text: 'Third Task'},
-];
+const toggleChecked = ({ _id, isChecked }) => {
+  TasksCollection.update(_id, {
+    $set: {
+      isChecked: !isChecked
+    }
+  })
+};
 
-export const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <ul>
-      { tasks.map(task => <Task key={ task._id } task={ task }/>) }
-    </ul>
-  </div>
-);
+const deleteTask = ({ _id }) => TasksCollection.remove(_id);
+ 
+export const App = () => {
+
+  const hideCompletedFilter = { isChecked: { $ne: true } };
+  const [hideCompleted, setHideCompleted] = useState(false);
+  const tasks = useTracker(() =>
+    TasksCollection.find(hideCompleted ? hideCompletedFilter : {}, {
+      sort: { createdAt: -1 },
+    }).fetch()
+  );
+ 
+  return (
+    <div className="app">
+      <header>
+        <div className="app-bar">
+          <div className="app-header">
+          <h1>📝️ To Do List</h1>
+          </div>
+        </div>
+      </header>
+
+      <div className="main">
+        <TaskForm />
+        <div className="filter">
+         <button onClick={() => setHideCompleted(!hideCompleted)}>
+           {hideCompleted ? 'Show All' : 'Hide Completed'}
+         </button>
+       </div>
+        <ul className="tasks">
+          {tasks.map(task => (
+            <Task
+              key={task._id}
+              task={task}
+              onCheckboxClick={toggleChecked}
+              onDeleteClick={deleteTask}
+            />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
