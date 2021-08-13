15ad8ec2fe14a3d04cf0e346a3b997f50cfcc2df
Andrew Klimenyuk <andrew.klmn@gmail.com>
2021-08-13 22:11:19 +0300

    Add: users and payments collections

--- server/main.js
+++ server/main.js
@@ -1,18 +1,30 @@
-import { Meteor } from 'meteor/meteor';
-import { TasksCollection } from '/imports/api/TasksCollection';
+import { Meteor } from "meteor/meteor";
+import { TasksCollection } from "/imports/api/TasksCollection";
+import { UsersCollection } from "/imports/api/UsersCollection";
+import { PaymentsCollection } from "/imports/api/PaymentsCollection";
 
-const insertTask = taskText => TasksCollection.insert({ text: taskText });
- 
-Meteor.startup(() => {
-  if (TasksCollection.find().count() === 0) {
-    [
-      'First Task',
-      'Second Task',
-      'Third Task',
-      'Fourth Task',
-      'Fifth Task',
-      'Sixth Task',
-      'Seventh Task'
-    ].forEach(insertTask)
+const insertRecord = (collection, object) => collection.insert(object);
+
+const insertArrayOfRecords = (collection, recordsArray) => {
+  if (collection.find().count() === 0) {
+    recordsArray.forEach((value) => insertRecord(collection, value));
   }
+};
+
+Meteor.startup(() => {
+  insertArrayOfRecords(TasksCollection, [
+    { text: "1 Task", createdAt: new Date().toISOString() },
+    { text: "2 Task", createdAt: new Date().toISOString() },
+    { text: "3 Task", createdAt: new Date().toISOString() },
+    { text: "4 Task", createdAt: new Date().toISOString() },
+  ]);
+
+  insertArrayOfRecords(UsersCollection, [
+    { login: "andrew", passwordHash: "49fe754f9af13d408daecf9dddd8d488", role: 'user', },
+    { login: "katya", passwordHash: "644b487c6850a29f67e20d936a613024", role: 'accountant' },
+  ]);
+
+  insertArrayOfRecords(PaymentsCollection, [
+    { createdAt: new Date().toISOString(), userId: 'andrewsid', income: 0.0, expence: 0.0 },
+  ]);
 });
